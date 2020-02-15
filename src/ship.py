efb9556c5fc3a51569703daa848fec9e65eea820
dbuiviet <dbuiviet@gmail.com>
2020-02-15 23:59:30 +0700

    143 DN

---
+++ src/ship.py
@@ -0,0 +1,42 @@
+import os
+import pygame
+from settings import Settings
+
+
+class Ship:
+    """A class to manage the ship."""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        super().__init__()
+        self.settings = ai_game.settings
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect
+        self.image = pygame.image.load(os.path.join('src/images', 'ship.bmp'))
+        self.image_rect = self.image.get_rect()
+        # Start each new ship at the bottom center of the screen.
+        self.image_rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.image_rect.x)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's x value, not the rect
+        if self.moving_right and (self.image_rect.right < self.screen_rect.right):
+            self.x += self.settings.ship_speed
+        elif self.moving_left and (self.image_rect.left > 0):
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.image_rect.x = self.x
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.image_rect)
